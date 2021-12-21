a5acc410912b468d694e7fe84abcccbc391abfac
Guilherme Cavassani <guilhermecavassani@SEO-MacBook-Pro.local>
2021-12-21 17:41:57 -0300

    instala nuxt content

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1> Teste de titulo </h1> 
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style> 
+  h1 {font-family: "helvetia";}
+</style>
